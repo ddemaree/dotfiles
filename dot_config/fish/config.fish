de578e335fc782942b13a734de370a89e0cb4221
David Demaree <david@demaree.me>
2026-02-25 07:38:37 -0500

    Update fish config

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,8 +1,22 @@
 # ~/.config/fish/config.fish
 
+set -g fish_greeting
+set -gx EDITOR nvim
+
 if status is-interactive
-  starship init fish | source
-  zoxide init fish | source
-  ~/.local/bin/mise activate fish | source
-  tv init fish | source
+    # Commands to run in interactive sessions can go here
+    set -gx PNPM_HOME "$HOME/.local/share/pnpm"
+    fish_add_path $PNPM_HOME
+    fish_add_path ~/.local/bin
+
+    alias la='eza -la'
+    alias ll='eza -l'
+    alias moi='chezmoi'
+
+    starship init fish | source
+    mise activate fish | source
+    zoxide init fish | source
+    fzf --fish | source
+    direnv hook fish | source
+    tv init fish | source
 end
